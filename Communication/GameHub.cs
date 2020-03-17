4fbcba33024d4222c55a9a0026dc193dc4a5a6a5
Lorelei Augeraud <lorelei.augeraud@u-psud.fr>
2020-03-17 20:41:40 +0100

    [PWeeb] Commit Connection Inscription

--- Communication/GameHub.cs
+++ Communication/GameHub.cs
@@ -12,45 +12,45 @@ namespace ProjectWeeb.Communication
 {
     public class GameHub : Hub
     {
-        public async Task SendMessage(string user, string message)
-        {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
-        }
+        //public async Task SendMessage(string user, string message)
+        //{
+        //    await Clients.All.SendAsync("ReceiveMessage", user, message);
+        //}
 
-        /// <summary>
-        /// Essaie de connecter le player au jeu
-        /// </summary>
-        /// <param name="player">Joueur à connecter</param>
-        /// <returns></returns>
-        public async Task ConnectPlayer(Player player)
-        {
-            await GameManager.GetInstance().LogInPlayer(player);
-        }
+        ///// <summary>
+        ///// Essaie de connecter le player au jeu
+        ///// </summary>
+        ///// <param name="player">Joueur à connecter</param>
+        ///// <returns></returns>
+        //public async Task ConnectPlayer(Player player)
+        //{
+        //    await GameManager.GetInstance().LogInPlayer(player);
+        //}
 
-        public async Task DisconnectPlayer(Player player)
-        {
-            await GameManager.GetInstance().DisconnectPlayer(player);
-        }
+        //public async Task DisconnectPlayer(Player player)
+        //{
+        //    await GameManager.GetInstance().DisconnectPlayer(player);
+        //}
 
-        public async Task PlayCard(Player player, Card card, Location location)
-        {
-            await GameManager.GetInstance().PlayCard(player, card, location);
-        }
+        //public async Task PlayCard(Player player, Card card, Location location)
+        //{
+        //    await GameManager.GetInstance().PlayCard(player, card, location);
+        //}
 
-        public async Task DealDamageToOpponent(Card card, Player receiver)
-        {
-            await GameManager.GetInstance().DealDamageToOpponent(card, receiver);
-        }
+        //public async Task DealDamageToOpponent(Card card, Player receiver)
+        //{
+        //    await GameManager.GetInstance().DealDamageToOpponent(card, receiver);
+        //}
         
-        public async Task DealDamageToCard(Player receiver, Card cardDealer, Card cardReceiver)
-        {
-            await GameManager.GetInstance().DealDamageToCard(receiver, cardDealer, cardReceiver);
-        }
+        //public async Task DealDamageToCard(Player receiver, Card cardDealer, Card cardReceiver)
+        //{
+        //    await GameManager.GetInstance().DealDamageToCard(receiver, cardDealer, cardReceiver);
+        //}
 
-        public async Task ActivateCardEffect(Card card, Effect effect)
-        {
-            await GameManager.GetInstance().ActivateCardEffect(card, effect);
-        }
+        //public async Task ActivateCardEffect(Card card, Effect effect)
+        //{
+        //    await GameManager.GetInstance().ActivateCardEffect(card, effect);
+        //}
 
 
     }
