2243c2cec77a6806ae61abcdd439207535d7b026
Fraaktal <50698889+Fraaktal@users.noreply.github.com>
2020-06-08 18:02:16 +0200

    [PW] Interface

--- Pages/GamePlateau.cshtml.cs
+++ Pages/GamePlateau.cshtml.cs
@@ -1,14 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.SignalR.Client;
-using Newtonsoft.Json;
-using ProjectWeeb.GameCard.Business.BusinessData;
-using ProjectWeeb.GameCard.Manager;
 
 namespace ProjectWeeb.Pages
 {
